233e82f33ee3a2a3b44310cab0f877b10b450873
Alan Xu <xuyi7126@gmail.com>
2024-12-13 23:49:33 +1100

    theme

--- my_profile/src/app/page.tsx
+++ my_profile/src/app/page.tsx
@@ -1,12 +1,5 @@
 
 
 export default function Home() {
-  return (
-    <div>
-      <h1>Hello, I'm Shiyan Xu</h1>
-      <p>
-        Welcome to my world! /n
-      </p>
-    </div>
-  );
+  return <main className=""></main>
 }
