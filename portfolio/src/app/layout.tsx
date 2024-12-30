aef2fe91657ac9b680857a356f9088fc704be465
Alan Xu <xuyi7126@gmail.com>
2024-12-30 20:37:33 +1100

    header

--- portfolio/src/app/layout.tsx
+++ portfolio/src/app/layout.tsx
@@ -1,7 +1,12 @@
-import Header from "@/componenet/header"
+import Header from "@/components/header"
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ActiveSectionContextProvider from "@/context/active-section-context";
+import Footer from "@/components/footer";
+import ThemeSwitch from "@/components/theme-switch";
+import ThemeContextProvider from "@/context/theme-context";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,10 +29,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="!scroll-smooth">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased
-          bg-violet-50 text-gray-950 relative h-[5000px]`}
+          bg-violet-50 text-gray-950 h-[5000px]`}
       >
         <div className="bg-blue-200 absolute top-[-6rem] -z-10 right-[11rem]
         h-[32rem] w-[32rem] rounded-full blur-[10rem] sm:w-[68rem]"></div>
@@ -35,8 +40,8 @@ export default function RootLayout({
         h-[32rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68rem]
         md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
        
-        
-
+        <Header />
+        <main>{children}</main>
       </body>
     </html>
   );
---
+++ portfolio/src/components/footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Footer() {
+    <footer className="mb-10 px-4 text-center text-gray-500">
+        <small className="mb-2 block text-xs">
+            &copy; 2035 Junji. All rights reserved.
+        </small>
+        < p className="text-xs">
+            <span className="font-semibold">About this website:</span> build with
+            React & Next.js (App Router & Server Action), TypeScript, Tailwind CSS,
+            Frame Motion, React Email & Resend, Vercel hosting.
+        </p>
+    </footer>
+}
