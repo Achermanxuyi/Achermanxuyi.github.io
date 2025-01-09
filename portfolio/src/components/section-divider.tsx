2890946fe321d61e5ea4c4aa15eb1a0d7414ca24
Alan Xu <xuyi7126@gmail.com>
2025-01-10 00:04:33 +1100

    framework done

--- portfolio/src/components/section-divider.tsx
+++ portfolio/src/components/section-divider.tsx
@@ -6,7 +6,7 @@ import { motion } from "motion/react";
 export default function SectionDivider() {
     return (
         <motion.div
-            className="bg-gray-950 my-16 h-24 w-1 rounded-full hidden sm:block dark:bg-opacity-20"
+            className="bg-gray-950 my-12 h-24 w-1 rounded-full hidden sm:block dark:bg-opacity-20"
             initial={{ opacity: 0, y: 100 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ delay: 0.125 }}
